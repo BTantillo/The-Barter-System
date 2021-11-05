e019df4ef0c7fad0afb4a753ae65263c576034f1
Miranda <88161424+MirandaT77@users.noreply.github.com>
2021-11-04 23:16:44 -0700

    tested files with workbench

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,9 @@
-INSERT INTO users (first_name, last_name, location)
+INSERT INTO user (first_name, last_name, location)
 VALUES 
+('Maureen', 'Carpenter', 'Gold Beach'),
 ('Miranda', 'Thompson', 'Brookings');
 
 INSERT INTO items_available (item, description)
 VALUES
-('Guitar', 'Acoustic guitar, needs tuning and has some wear, but still has a lot of life left!');
+('Guitar', 'Acoustic guitar, needs tuning and has some wear, but still has a lot of life left!'),
 ('Tricycle', 'My son outgrew this, practically new, message me if interested');
