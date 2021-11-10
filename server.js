eacf120572553343041bb69f092e324db78ce9c3
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-09 19:46:00 -0800

    safe spot

--- server.js
+++ server.js
@@ -1,35 +1,46 @@
+const path = require("path");
 const express = require("express");
-//const db = require("./config/connection");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 
-// const apiRoutes = require('./routes/apiRoutes');
-
-//const sequelize = require("./config/connection");
-
-const PORT = 3006 || process.env.PORT;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Express middleware
-//app.use(express.urlencoded({ extended: false }));
-//app.use(express.json());
-// app.use('/api', apiRoutes);
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-//Server test
-app.get("/", (req, res) => {
-  res.json({
-    message: "Bing Bong",
-  });
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
+
+//sets up Handlebars.js as apps template engine of choice
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); //!14.5.7 snap changed to false
+//express.static() method is a built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets
+app.use(express.static(path.join(__dirname, "public")));
+
+const routes = require("./controllers/");
+
+// turn on routes
+app.use(routes); //Code snap  14.1 does not list
+
+// turn on connection to db and server
+//! To reset the db force: true. Otherwise default to false.
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
-
-// Not Found response for unmatched routes
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-// Start server after DB connection
-// app.connect((err) => {
-//   if (err) throw err;
-//   console.log("Database is connected");
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-// });
