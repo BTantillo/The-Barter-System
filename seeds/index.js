53e4f41d8e82cbc24be1361f04933710790c149a
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-09 20:03:21 -0800

    files transfer

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,24 @@
-const seeds = require("./seeds");
+const seedUsers = require('./user-seeds');
+const seedPosts = require('./post-seeds');
+const seedComments = require('./comment-seeds');
+const seedVotes = require('./vote-seeds');
 
-const sequelize = require("../config/connection");
+const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log("--------------");
-  await seeds();
-  console.log("--------------");
-  // await seedPosts(); DUPLICATE this line for each seed file needed
-  console.log("--------------");
+  console.log('--------------');
+  await seedUsers();
+  console.log('--------------');
+
+  await seedPosts();
+  console.log('--------------');
+
+  await seedComments();
+  console.log('--------------');
+
+  await seedVotes();
+  console.log('--------------');
 
   process.exit(0);
 };
