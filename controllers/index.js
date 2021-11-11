3ea05d14efc2b736485547b4095f155687f7205e
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-10 18:58:16 -0800

    Working code, pages showing

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+const apiRoutes = require("./api"); //! This looks for an index.js file
+const homeRoutes = require("./home-routes");
+const dashboardRoutes = require("./dashboard-routes");
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/dashboard", dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
