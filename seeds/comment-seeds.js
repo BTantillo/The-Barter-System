1007e59c2c17cb49bddd94e4bdf55776290eba63
Miranda <88161424+MirandaT77@users.noreply.github.com>
2021-11-12 14:16:40 -0800

    Updated comment seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,263 +2,252 @@ const { Comment } = require('../models');
 
 const commentdata = [
   {
-    comment_text: 'Nunc rhoncus dui vel sem.',
+    comment_text: 'I have an electric guitar I am willing to trade. Let me know.',
     user_id: 6,
     post_id: 1
   },
   {
-    comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    comment_text: 'I have a 2 wheel drive quad Id be willing to part with',
     user_id: 6,
     post_id: 8
   },
   {
-    comment_text: 'Aliquam erat volutpat. In congue.',
+    comment_text: 'I have a couple horses, I could show you some basic things.',
     user_id: 3,
     post_id: 10
   },
   {
-    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    comment_text: 'I have a salt water tank Id be willing to part with.',
     user_id: 3,
     post_id: 18
   },
   {
-    comment_text: 'In hac habitasse platea dictumst.',
+    comment_text: 'Still Available',
     user_id: 7,
     post_id: 5
   },
   {
-    comment_text: 'Vivamus vestibulum sagittis sapien.',
+    comment_text: 'My kids would love this! FIRST PLEASE!!',
     user_id: 1,
     post_id: 20
   },
   {
-    comment_text: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    comment_text: 'I will take as many tomatoes as I can, making homemade pasta sauce, this will be perfect!',
     user_id: 6,
     post_id: 7
   },
   {
-    comment_text: 'Sed vel enim sit amet nunc viverra dapibus.',
+    comment_text: 'I would love some lessons, I can help with yard work.',
     user_id: 7,
     post_id: 4
   },
   {
-    comment_text: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    comment_text: 'Do you have a webpage where I can see your work? I am interested in matching bracelets for my aunts for Christmas.',
     user_id: 6,
     post_id: 12
   },
   {
-    comment_text: 'Morbi a ipsum.',
+    comment_text: 'Second please! I can pick up.',
     user_id: 6,
     post_id: 20
   },
   {
-    comment_text: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    comment_text: 'Ready to get rid of this thing FAST',
     user_id: 3,
     post_id: 14
   },
   {
-    comment_text: 'Donec ut mauris eget massa tempor convallis.',
+    comment_text: 'Would you be interested in offering catering?',
     user_id: 5,
     post_id: 4
   },
   {
-    comment_text:
-      'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+    comment_text: 'I also have a collection I am looking to add to, can I come take a look at what you have?',
     user_id: 4,
     post_id: 9
   },
   {
-    comment_text:
-      'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
+    comment_text: 'I am interested, could we meet at a jeweler so I can get an appraisal?',
     user_id: 5,
     post_id: 14
   },
   {
-    comment_text: 'Quisque porta volutpat erat.',
+    comment_text: 'I have ALWAYS wanted to learn. Do you have a board or do I need to buy or rent one? What else would I need?',
     user_id: 6,
     post_id: 2
   },
   {
-    comment_text: 'Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
+    comment_text: 'I have a beginners surf board you can use, however you may need to rent a wetsuit, the water is cold!',
     user_id: 8,
     post_id: 2
   },
   {
-    comment_text:
-      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
+    comment_text: 'Next please! If the others fall through',
     user_id: 2,
     post_id: 20
   },
   {
-    comment_text: 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',
+    comment_text: 'Can you please send some pics of the interior to my email cstoneman2@last.fm Thanx' ,
     user_id: 4,
     post_id: 11
   },
   {
-    comment_text:
-      'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.',
+    comment_text: 'What sort of limitations do you have? I would like my property surveyed.',
     user_id: 5,
     post_id: 13
   },
   {
-    comment_text:
-      'Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
+    comment_text: 'I would like this lot',
     user_id: 9,
     post_id: 16
   },
   {
-    comment_text: 'Curabitur convallis.',
+    comment_text: 'I would love to learn how to make some spanish cuisine, can you help?',
     user_id: 6,
     post_id: 4
   },
   {
-    comment_text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    comment_text: 'I have some donkeys you can ride LOL',
     user_id: 4,
     post_id: 10
   },
   {
-    comment_text: 'Morbi non quam nec dui luctus rutrum.',
+    comment_text: 'I have a 4x4 quad, needsa some work though',
     user_id: 3,
     post_id: 8
   },
   {
-    comment_text:
-      'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique.',
+    comment_text: 'I know of an equestrian center, I will see if they are interested in barters',
     user_id: 8,
     post_id: 10
   },
   {
-    comment_text:
-      'Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.',
+    comment_text: 'Which venu is this at?',
     user_id: 1,
     post_id: 15
   },
   {
-    comment_text: 'Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend.',
+    comment_text: 'I would love to take you up on your offer please',
     user_id: 5,
     post_id: 3
   },
   {
-    comment_text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    comment_text: 'Are you interested in trading for all 4 tickets?',
     user_id: 1,
     post_id: 15
   },
   {
-    comment_text: 'Nam tristique tortor eu pede.',
+    comment_text: 'Second please, my sister is due any time and needs clothing badly',
     user_id: 4,
     post_id: 16
   },
   {
-    comment_text: 'Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
+    comment_text: 'Im really interested in a salt water tank, can you email me?',
     user_id: 4,
     post_id: 18
   },
   {
-    comment_text: 'Proin eu mi. Nulla ac enim.',
+    comment_text: 'Im joking!',
     user_id: 4,
     post_id: 10
   },
   {
-    comment_text: 'Sed ante. Vivamus tortor.',
+    comment_text: 'Open to trades???',
     user_id: 7,
     post_id: 5
   },
   {
-    comment_text: 'Aliquam quis turpis eget elit sodales scelerisque.',
+    comment_text: 'No, only looking for acoustic, thank you though',
     user_id: 10,
     post_id: 1
   },
   {
-    comment_text: 'Donec quis orci eget orci vehicula condimentum.',
+    comment_text: 'I have a whole garage of things that need fixing, I also have some items for barter.',
     user_id: 3,
     post_id: 19
   },
   {
-    comment_text: 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    comment_text: 'Next please, I have been looking for this size.',
     user_id: 5,
     post_id: 3
   },
   {
-    comment_text: 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.',
+    comment_text: 'Do you have any paperwork on the ring? Appraisals, etc?',
     user_id: 10,
     post_id: 14
   },
   {
-    comment_text: 'Maecenas ut massa quis augue luctus tincidunt.',
+    comment_text: 'Are you willing to trade snything else?',
     user_id: 10,
     post_id: 8
   },
   {
-    comment_text: 'Cras in purus eu magna vulputate luctus.',
+    comment_text: 'I have a GMC Yukon I may trade you for. When are you available?',
     user_id: 10,
     post_id: 11
   },
   {
-    comment_text: 'Etiam vel augue. Vestibulum rutrum rutrum neque.',
+    comment_text: 'I need some tires for my 4runner, will they fit?',
     user_id: 8,
     post_id: 5
   },
   {
-    comment_text: 'Proin at turpis a pede posuere nonummy.',
+    comment_text: 'I need my law edger fixed ASAP for my business, I can trade yardwork',
     user_id: 8,
     post_id: 19
   },
   {
-    comment_text: 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.',
+    comment_text: 'I have a couple broken weedeaters that I need fixed up so I can sell.',
     user_id: 9,
     post_id: 19
   },
   {
-    comment_text:
-      'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
+    comment_text: 'If not, could you make a recommendation for a caterer?',
     user_id: 5,
     post_id: 4
   },
   {
-    comment_text:
-      'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.',
+    comment_text: 'I am looking for a 4x4, but only have a sedan for trade. Any interest? Great commuter car, excellent on gas. ',
     user_id: 2,
     post_id: 11
   },
   {
-    comment_text: 'Vestibulum ac est lacinia nisi venenatis tristique.',
+    comment_text: 'Preferably at my house',
     user_id: 4,
     post_id: 6
   },
   {
-    comment_text: 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    comment_text: 'I am willing to baysit at my home, I watch other kids so it cannot be at your house',
     user_id: 9,
     post_id: 6
   },
   {
-    comment_text:
-      'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam.',
+    comment_text: 'Do you have any Elvis records?',
     user_id: 7,
     post_id: 9
   },
   {
-    comment_text: 'Integer ac leo. Pellentesque ultrices mattis odio.',
+    comment_text: 'Can you fix home appliances?',
     user_id: 4,
     post_id: 19
   },
   {
-    comment_text: 'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
+    comment_text: 'Well, let me think about it, I will let you know this week',
     user_id: 10,
     post_id: 1
   },
   {
-    comment_text:
-      'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.',
+    comment_text: 'I run a shop in town, are you looking for work? I need someone ASAP',
     user_id: 2,
     post_id: 19
   },
   {
-    comment_text: 'Proin risus. Praesent lectus.',
+    comment_text: 'Sounds good, lets meet up so I can see this guitar',
     user_id: 10,
     post_id: 1
   },
   {
-    comment_text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus.',
+    comment_text: 'I am also interested, I would love to have a matching set made for my mother',
     user_id: 10,
     post_id: 12
   }
