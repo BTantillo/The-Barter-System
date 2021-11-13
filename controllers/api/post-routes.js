5e935bab64d020076a5484668423584759ac1c63
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-12 19:49:24 -0800

    reseting my branch I got freaked out.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const sequelize = require("../../config/connection");
 const { Post, User, Comment, Vote } = require("../../models");
 const withAuth = require("../../utils/auth");
+const multer  = require('multer');
 const path = require("path");
 
 //!Incoming comment, When you do your pull this file will be heavily edited.
@@ -21,7 +22,7 @@ const storage = multer.diskStorage({
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     //file.original name retains the original file name
     cb(null, file.originalname);
-  },
+  }
 });
 
 const upload = multer({ storage: storage });
@@ -78,6 +79,7 @@ router.get("/", (req, res) => {
       "id",
       "post_url",
       "title",
+      "description",
       "created_at",
       [
         sequelize.literal(
@@ -89,7 +91,7 @@ router.get("/", (req, res) => {
     include: [
       {
         model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        attributes: ["id", "description", "comment_text", "post_id", "user_id", "created_at"],
         include: {
           model: User,
           attributes: ["username"],
@@ -117,6 +119,7 @@ router.get("/:id", (req, res) => {
       "id",
       "post_url",
       "title",
+      "description",
       "created_at",
       [
         sequelize.literal(
@@ -128,7 +131,7 @@ router.get("/:id", (req, res) => {
     include: [
       {
         model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        attributes: ["id", "comment_text", "description", "post_id", "user_id", "created_at"],
         include: {
           model: User,
           attributes: ["username"],
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3006;
 
