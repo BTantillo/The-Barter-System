d31c11d3e57c19b43d83a93e885d787a662f0566
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-13 11:30:20 -0800

    after tutoring session

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -5,12 +5,14 @@ const postdata = [
     title: 'WANTED: Guitar',
     description: 'Acoustic guitar in working condition wanted. Have womens clothing, mens golf set or soccer net available for trade.',
     post_url: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+    file_name: "kitten1.jpeg",
     user_id: 10
   },
   {
     title: 'AVAILABLE: Surf Lessons',
     description: 'I an offering surf lessons, I am an experienced instructor with competition experience and can safely train children and adults. Open to offers!',
     post_url: 'https://nasa.gov/donec.json',
+    file_name: "kitten1.jpeg",
     user_id: 8
   },
   {
@@ -23,102 +25,119 @@ const postdata = [
     title: 'Cooking lessongs offered',
     description: 'Gourmet cook and chef ready to help you learn! I really need someone to change the oil in my car for trade. Thank you!',
     post_url: 'http://desdev.cn/enim/blandit/mi.jpg',
+    file_name: "kitten1.jpeg",
     user_id: 4
   },
   {
     title: 'AVAILABLE: Tires',
     description: 'I have tires taken off my stock tacoma, 60% tread, open to trades',
     post_url: 'http://google.ca/nam/nulla/integer.aspx',
+    file_name: "kitten1.jpeg",
     user_id: 7
   },
   {
     title: 'WANTED: Babysitter',
     description: 'Desparetly needing a babysitter for my 5 year old daughter all day on Sunday and after school Monday thru Wednesdays, I am able to cook for you, clean your home or walk your dog. Please message me.',
     post_url: 'https://stanford.edu/consequat.png',
+    file_name: "kitten1.jpeg",
     user_id: 4
   },
   {
     title: 'FREE: TOMATOESSS!!!',
     description: 'My garden is overflowing, I have tomatoes out my ears, please come take some off my hands!',
     post_url: 'http://edublogs.org/non/ligula/pellentesque.js',
+    file_name: "kitten1.jpeg",
     user_id: 1
   },
   {
     title: 'WANT TO TRADE: Wanting to trade my dirt bike for quad',
     description: 'Ive got a 2003 Honda CR 85R dirt bike that Id like to trade for a 4x4 quad. Bike runs great, just have no where to ride it and would like to try a quad for hunting ',
     post_url: 'http://ucla.edu/consequat/nulla.html',
+    file_name: "kitten1.jpeg",
     user_id: 1
   },
   {
     title: 'Record Collection: Open to trades',
     description: 'I have 300 records ranging from country, to rock, to oldies and more. I am open to trades for interested parties',
     post_url: 'http://theguardian.com/dui/vel/nisl/duis/ac/nibh.aspx',
+    file_name: "kitten1.jpeg",
     user_id: 9
   },
   {
     title: 'Horse-rising Lessons WANTED',
     description: 'I want to learn to ride horses! Please teach me, I have many household items I can trade such as a King size bed frame, Lawnmower, Desk and chair, and more. Lets make a deal!',
     post_url: 'https://reverbnation.com/ligula/sit.jpg',
+    file_name: "kitten1.jpeg",
     user_id: 5
   },
   {
     title: 'TRADING my 1998 Ford F250 for ???',
     description: 'I have a 1998 Ford F250 with 255,000 miles on it, works well, paint faded, will trade for guns, motorcycles, boats, etc. Whatcha got?',
     post_url: 'http://china.com.cn/lectus/vestibulum.json',
+    file_name: "kitten1.jpeg",
     user_id: 3
   },
   {
     title: 'AVAILABLE: Homemade jewelry',
     description: 'I handmake earrings, necklaces, bracelets, anklets and more. I would be happy to trade, let me know what you want and what you have.',
     post_url: 'http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json',
+    file_name: "kitten1.jpeg",
     user_id: 10
   },
   {
     title: 'Drone services',
     description: 'I am a skilled drone operator, I can help you with your projects from real estate to surveillance. I am open to goods or services',
     post_url: 'https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml',
+    file_name: "kitten1.jpeg",
     user_id: 8
   },
   {
     title: 'AVAILABLE: Engagement ring',
     description: 'I have a beautiful engagnement ring, white gold 1.5 karat diamond. Must see to appreciate. NO longer want or need. Fianccee is a cheater!!! Make me an offer! ',
     post_url: 'https://lycos.com/natoque/penatibus/et.html',
+    file_name: "kitten1.jpeg",
     user_id: 3
   },
   {
     title: 'Tickets to concert AVAILABLE',
     description: 'Bought 4 tickets to the Blake Shelton concert, 2 invites cannot go, so 2 tickets, front row are avilable. I am really looking for services such as automotive, landscaping, or ???',
     post_url: 'https://gmpg.org/lorem.jpg',
+    file_name: "kitten1.jpeg",
     user_id: 3
   },
   {
     title:'AVAILABLE: Lot of Infant girl clothing',
     description: 'I have girls 0-3 month clothing, an entire lot of jammies, outfits, onesies, etc. I am looking for childrens clothing and womens shoes size 8.',
     post_url: 'https://paginegialle.it/mattis/egestas.jsp',
+    file_name: "kitten1.jpeg",
     user_id: 7
   },
   {
     title: 'FREE: Notebooks',
     description: 'I bought too may notebooks for school. I have 5 available for free. Just want gone',
     post_url: 'http://wikia.com/turpis/eget.jpg',
+    file_name: "kitten1.jpeg",
     user_id: 6
   },
   {
     title: 'WANTED: Fish tank',
     description: 'Wanting a 10 gallon+ size fish tank, I have a decent coin collection to trade or some firewood',
     post_url: 'https://shareasale.com/quis.json',
+    file_name: "kitten1.jpeg",
     user_id: 4
   },
   {
     title: 'OFFER: Small engine repair',
     description: 'Open to trades of all kind, I can do small engine repairs on weedeaters, lawnmowers, motorcycles, quads, etc.',
     post_url: 'http://java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices.png',
+    file_name: "kitten1.jpeg",
     user_id: 6
   },
   {
     title: 'FREE: trampoline',
     description: 'Trampoline is 3 years old, still in good condition with net. We are moving and cannot take. First come, first serve!',
     post_url: 'https://java.com/at/nibh/in.png',
+    file_name: "kitten1.jpeg",
     user_id: 7
   }
 ];
